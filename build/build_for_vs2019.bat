b28091e1b564f876a01b249d726cd429cffee731
zero <zero.kwok@foxmail.com>
2021-02-22 17:18:00 +0800

    package添加utility_INCLUDE_DIRS, utility_LIBRARY_DIRS, utility_SOURCE_DIR三个cmake变量

--- build/build_for_vs2019.bat
+++ build/build_for_vs2019.bat
@@ -1,44 +1,68 @@
+@echo off
+set ProjectName=utility
+set ProjectVersion=0.2.1
+set ProjectGenerator32Bit="Visual Studio 16 2019" -A Win32
+set ProjectGenerator64Bit="Visual Studio 16 2019" -A x64
+set ProjectLibSufix=vc14.2
+set ProjectDependPath=G:\local\libraries\boost\boost_1_69_0-msvc-14.1
+
+
+REM 生成静态运行时32位版本
+
+echo on
 mkdir build_vs2019
 pushd build_vs2019
-cmake -G "Visual Studio 16 2019" -A Win32 ../.. -DCMAKE_INSTALL_PREFIX="../utility_0.2.1_static_x86_vc14.2" -DUTILITY_ENABLE_STATIC_RUNTIME=ON -DUTILITY_BUILD_TEST=OFF -DCMAKE_PREFIX_PATH="G:\local\libraries\boost\boost_1_69_0-msvc-14.1"
-cmake --build . --config Debug
-cmake --build . --config RelWithDebInfo
+cmake -G %ProjectGenerator32Bit% ../.. -DCMAKE_INSTALL_PREFIX:PATH="../%ProjectName%_%ProjectVersion%_static_x86_%ProjectLibSufix%" -DUTILITY_ENABLE_STATIC_RUNTIME=ON -DUTILITY_BUILD_TEST=OFF -DCMAKE_PREFIX_PATH:STRING="%ProjectDependPath%"
+cmake --build   . --config Debug
+cmake --build   . --config RelWithDebInfo
 cmake --install . --config Debug
 cmake --install . --config RelWithDebInfo
 popd
 del /S /Q .\build_vs2019
 rd /S /Q .\build_vs2019
 
+
+REM 生成动态运行时32位版本
+
+echo on
 mkdir build_vs2019
 pushd build_vs2019
-cmake -G "Visual Studio 16 2019" -A Win32 ../.. -DCMAKE_INSTALL_PREFIX="../utility_0.2.1_dynamic_x86_vc14.2" -DUTILITY_ENABLE_STATIC_RUNTIME=OFF -DUTILITY_BUILD_TEST=OFF -DCMAKE_PREFIX_PATH="G:\local\libraries\boost\boost_1_69_0-msvc-14.1"
-cmake --build . --config Debug
-cmake --build . --config RelWithDebInfo
+cmake -G %ProjectGenerator32Bit% ../.. -DCMAKE_INSTALL_PREFIX:PATH="../%ProjectName%_%ProjectVersion%_dynamic_x86_%ProjectLibSufix%" -DUTILITY_ENABLE_STATIC_RUNTIME=OFF -DUTILITY_BUILD_TEST=OFF -DCMAKE_PREFIX_PATH:STRING="%ProjectDependPath%"
+cmake --build   . --config Debug
+cmake --build   . --config RelWithDebInfo
 cmake --install . --config Debug
 cmake --install . --config RelWithDebInfo
 popd
 del /S /Q .\build_vs2019
 rd /S /Q .\build_vs2019
 
+REM 生成静态运行时64位版本
+
+echo on
 mkdir build_vs2019
 pushd build_vs2019
-cmake -G "Visual Studio 16 2019" -A x64 ../.. -DCMAKE_INSTALL_PREFIX="../utility_0.2.1_static_x64_vc14.2" -DUTILITY_ENABLE_STATIC_RUNTIME=ON -DUTILITY_BUILD_TEST=OFF -DCMAKE_PREFIX_PATH="G:\local\libraries\boost\boost_1_69_0-msvc-14.1"
-cmake --build . --config Debug
-cmake --build . --config RelWithDebInfo
+cmake -G %ProjectGenerator64Bit% ../.. -DCMAKE_INSTALL_PREFIX:PATH="../%ProjectName%_%ProjectVersion%_static_x64_%ProjectLibSufix%" -DUTILITY_ENABLE_STATIC_RUNTIME=ON -DUTILITY_BUILD_TEST=OFF -DCMAKE_PREFIX_PATH:STRING="%ProjectDependPath%"
+cmake --build   . --config Debug
+cmake --build   . --config RelWithDebInfo
 cmake --install . --config Debug
 cmake --install . --config RelWithDebInfo
 popd
 del /S /Q .\build_vs2019
 rd /S /Q .\build_vs2019
 
+
+REM 生成动态运行时64位版本
+
+echo on
 mkdir build_vs2019
 pushd build_vs2019
-cmake -G "Visual Studio 16 2019" -A x64 ../.. -DCMAKE_INSTALL_PREFIX="../utility_0.2.1_dynamic_x64_vc14.2" -DUTILITY_ENABLE_STATIC_RUNTIME=OFF -DUTILITY_BUILD_TEST=OFF -DCMAKE_PREFIX_PATH="G:\local\libraries\boost\boost_1_69_0-msvc-14.1"
-cmake --build . --config Debug
-cmake --build . --config RelWithDebInfo
+cmake -G %ProjectGenerator64Bit% ../.. -DCMAKE_INSTALL_PREFIX:PATH="../%ProjectName%_%ProjectVersion%_dynamic_x64_%ProjectLibSufix%" -DUTILITY_ENABLE_STATIC_RUNTIME=OFF -DUTILITY_BUILD_TEST=OFF -DCMAKE_PREFIX_PATH:STRING="%ProjectDependPath%"
+cmake --build   . --config Debug
+cmake --build   . --config RelWithDebInfo
 cmake --install . --config Debug
 cmake --install . --config RelWithDebInfo
 popd
 del /S /Q .\build_vs2019
 rd /S /Q .\build_vs2019
+
 
