f50d9a34fd87b4c7bf58c5e8953c1c2c5ecb8714
zero <zero.kwok@foxmail.com>
2021-06-18 11:21:50 +0800

    优化install_pdb()

--- cmake/internal_util.cmake
+++ cmake/internal_util.cmake
@@ -1,22 +1,21 @@
 function(install_pdb target_name)
-    
-    get_target_property(target_pdb_name_debug       ${target_name} COMPILE_PDB_NAME_DEBUG)
-    get_target_property(target_pdb_name_release     ${target_name} COMPILE_PDB_NAME_RELEASE)
-    get_target_property(target_pdb_output_directory ${target_name} PDB_OUTPUT_DIRECTORY)
-    
-    # 需要设置 set(CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/cmake")
-    # message("target_pdb_name_debug=${target_pdb_name_debug}")
-    # message("target_pdb_output_directory=${target_pdb_output_directory}")
 
-    # 在vs2019中, 文件输出目录为CMAKE_ARCHIVE_OUTPUT_DIRECTORY,
-    # 在cmd中则是CMAKE_ARCHIVE_OUTPUT_DIRECTORY/CMAKE_INSTALL_CONFIG_NAME
-    install(FILES
-        "${target_pdb_output_directory}/\${CMAKE_INSTALL_CONFIG_NAME}/$<$<CONFIG:Debug>:${target_pdb_name_debug}>$<$<NOT:$<CONFIG:Debug>>:${target_pdb_name_release}>.pdb"
-        DESTINATION lib
-        OPTIONAL)
+    #set_target_properties(${PROJECT_NAME} PROPERTIES DEBUG_POSTFIX "d")
+    #set_target_properties(${PROJECT_NAME} PROPERTIES 
+    #PDB_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib"
+    #COMPILE_PDB_NAME_DEBUG  "${PROJECT_NAME}d"
+    #COMPILE_PDB_NAME_RELEASE ${PROJECT_NAME}
+    #COMPILE_PDB_NAME_MINSIZEREL ${PROJECT_NAME}
+    #COMPILE_PDB_NAME_RELWITHDEBINFO ${PROJECT_NAME})
 
-    install(FILES
-        "${target_pdb_output_directory}/$<$<CONFIG:Debug>:${target_pdb_name_debug}>$<$<NOT:$<CONFIG:Debug>>:${target_pdb_name_release}>.pdb"
-        DESTINATION lib
-        OPTIONAL)
+    get_target_property(PROJECT_PDB_NAME_DEBUG    ${target_name} COMPILE_PDB_NAME_DEBUG)
+    get_target_property(PROJECT_PDB_NAME_RELEASE  ${target_name} COMPILE_PDB_NAME_RELEASE)
+    get_target_property(PROJECT_PDB_DIRECTORY     ${target_name} PDB_OUTPUT_DIRECTORY)
+    set(PROJECT_PDB_NAME "$<$<CONFIG:Debug>:${PROJECT_PDB_NAME_DEBUG}>$<$<NOT:$<CONFIG:Debug>>:${PROJECT_PDB_NAME_RELEASE}>.pdb")
+    
+    # 在cmd中pdb输出目录为 ${PROJECT_PDB_DIRECTORY}/${CMAKE_INSTALL_CONFIG_NAME}
+    install(FILES "${PROJECT_PDB_DIRECTORY}/\${CMAKE_INSTALL_CONFIG_NAME}/${PROJECT_PDB_NAME}" DESTINATION lib OPTIONAL)
+
+    # 在vs2019中pdb输出目录为 ${PROJECT_PDB_DIRECTORY}/${CMAKE_INSTALL_CONFIG_NAME}
+    install(FILES "${PROJECT_PDB_DIRECTORY}/${PROJECT_PDB_NAME}" DESTINATION lib OPTIONAL)
 endfunction()
