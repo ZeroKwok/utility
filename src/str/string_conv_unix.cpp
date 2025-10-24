03a4030cefd2c0f5b50c7667cf787a7599fd744e
zero <zero.kwok@foxmail.com>
2025-10-24 10:55:54 +0800

    feat(str): 将字符串转换函数的参数类型从 std::string 改为 std::string_view

--- src/str/string_conv_unix.cpp
+++ src/str/string_conv_unix.cpp
@@ -14,32 +14,32 @@
 namespace UTILITY_NAMESPACE {
 
 std::wstring& utf8_to_wstring(
-    const std::string& input, std::wstring& output){
+    const std::string_view& input, std::wstring& output){
     return convert_with_iconv(input, output, "UTF-8", "WCHAR_T", false);
 }
 
 std::wstring& string_to_wstring(
-    const std::string& input, std::wstring& output){
+    const std::string_view& input, std::wstring& output){
     return utf8_to_wstring(input, output);
 }
 
 std::string& wstring_to_utf8(
-    const std::wstring& input, std::string& output){
+    const std::wstring_view& input, std::string& output){
     return convert_with_iconv(input, output, "WCHAR_T", "UTF-8", false);
 }
 
 std::string& wstring_to_string(
-    const std::wstring& input, std::string& output){
+    const std::wstring_view& input, std::string& output){
     return wstring_to_utf8(input, output);
 }
 
 std::string& utf8_to_string(
-    const std::string& input, std::string& output){
+    const std::string_view& input, std::string& output){
     return output = input;
 }
 
 std::string& string_to_utf8(
-    const std::string& input, std::string& output){
+    const std::string_view& input, std::string& output){
     return output = input;
 }
 
